91ba54c53382fa64d76d324cd94e8bd088432f19
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-06-29 15:46:28 +0100

    number 8 completed

--- marvel.sql
+++ marvel.sql
@@ -129,7 +129,10 @@ INSERT INTO people (name) VALUES ('Bart Simpson');
 
 DELETE FROM people WHERE name = 'Eric Cartman';
 
+SELECT * FROM people;
+
 -- 8. The cinema has just heard that they will be holding an exclusive midnight showing of 'Avengers: Infinity War'!! Create a new entry in the 'movies' table to reflect this.
 
+INSERT INTO movies (title, year, show_time) VALUES ('Avengers: Infinity War', 2019, '00:00');
 
 -- 9. The cinema would like to make the Iron Man movies a triple billing. Find out the show time of "Iron Man 2" and set the show time of "Iron Man 3" to start two hours later.
